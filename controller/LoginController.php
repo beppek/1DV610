c584732a848d6ef791dbb42b6420e332ca03863b
Beppe Karlsson <beppe@live.se>
2016-09-26 18:30:47 +0200

    removes accidentally copied vars

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -2,14 +2,6 @@
 
 
 class LoginController {
-	// private static $login = 'LoginView::Login';
-	// private static $logout = 'LoginView::Logout';
-	// private static $name = 'LoginView::UserName';
-	// private static $password = 'LoginView::Password';
-	// private static $cookieName = 'LoginView::CookieName';
-	// private static $cookiePassword = 'LoginView::CookiePassword';
-	// private static $keep = 'LoginView::KeepMeLoggedIn';
-	// private static $messageId = 'LoginView::Message';
 
     public function login() {
         var_dump($_POST);
