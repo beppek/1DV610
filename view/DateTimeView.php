96e6086d1756253453eaca10451cefcaf9d524c7
Beppe Karlsson <beppe@live.se>
2016-09-26 13:19:12 +0200

    Displays date and time

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -5,7 +5,7 @@ class DateTimeView {
 
 	public function show() {
 
-		$timeString = 'TODO, Write servertime here...';
+		$timeString = date("l") . ", the " . date("jS") . " of " . date("F Y") . ", The time is " . date("H:i");
 
 		return '<p>' . $timeString . '</p>';
 	}
