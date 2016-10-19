70cb85cdd5e22d0972797bc9f6ec1e50e6795fef
beppek <beppe@live.se>
2016-10-19 20:28:46 +0200

    fix wrong indentation in DateTimeView

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -5,9 +5,9 @@ class DateTimeView {
 	public function show() {
 
 		$weekday = date("l");
-        $dayOfMonth = date("jS");
-        $monthAndYear = date("F Y");
-        $time24Hour = date("H:i:s");
+		$dayOfMonth = date("jS");
+		$monthAndYear = date("F Y");
+		$time24Hour = date("H:i:s");
 		$timeString =  $weekday . ", the " . $dayOfMonth . " of " . $monthAndYear . ", The time is " . $time24Hour;
 
 		return '<p>' . $timeString . '</p>';
